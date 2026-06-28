b0140df761865a9e402eac0198baab3c245cace3
casperjwk <casperjwk@gmail.com>
2026-06-28 16:50:58 +0900

    fix: remove unused Navi import from PostDetail component

--- src/pages/PostDetail.jsx
+++ src/pages/PostDetail.jsx
@@ -1,9 +1,18 @@
-import { useParams, Link , Navi } from 'react-router'
+import { useParams, Link } from 'react-router'
 
-export default function PostDetail(posts, onDelete){
-    const post = posts.find((p) => p.id === Number(id));
+export default function PostDetail(posts, onDelete) {
+  const { id } = useParams();
 
-    if(!post){
-        return
-    }
+  const post = posts.find((p) => p.id === Number(id));
+
+  if (!post) {
+    return (
+      <div>
+        <h2>존재하지 않는 게시글입니다.</h2>
+        <Link to="/posts">
+          게시글 목록으로 돌아가기
+        </Link>
+      </div>
+    )
+  }
 }
