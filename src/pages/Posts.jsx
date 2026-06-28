9fe7f45e8c74cd76d4d73a755a1638c71053d748
casperjwk <casperjwk@gmail.com>
2026-06-28 13:19:43 +0900

    feat: post.jsx

---
+++ src/pages/Posts.jsx
@@ -0,0 +1,22 @@
+import { Link } from 'react-router'
+
+export default function Posts({ post }) {
+  return (
+    <section>
+      <h2>글목록</h2>
+      {Posts.length === 0 ? (
+        <p>글이 없습니다.</p>
+      ): (
+        <ul>
+          {post.map((post)=>(
+            <li key={post.id}>
+              <Link to={`/posts/${post.id}`}>
+                {post.title}
+              </Link>
+            </li>
+          ))}
+        </ul>
+      )}
+    </section>
+  )
+}
